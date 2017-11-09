6e319135b3e63a42317a8e40c76bec91ef9332d4
aranzagp <ara13veela@gmail.com>
2017-11-08 23:41:51 -0600

    Add expenses model

---
+++ db/migrate/20171023054058_create_expenses.rb
@@ -0,0 +1,12 @@
+class CreateExpenses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :expenses do |t|
+      t.float :amount
+      t.string :date
+      t.string :place
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/expense_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class ExpenseTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @expense = Expense.new(amount: 30.00)
+  end
+
+  test "should be valid" do
+    assert @expense.valid?
+  end
+
+  test "amount should be present" do
+    @expense.amount = nil
+    assert_not @expense.valid?
+  end
+
+end
